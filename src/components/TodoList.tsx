6b20f864fe70cc34bf6f8976d53e1f04d64b8c1e
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-25 11:34:04 -0500

    Creating a new component for list and list items

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ToDoList: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish course" }];
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ToDoList;
