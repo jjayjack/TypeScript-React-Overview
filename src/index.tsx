4f896c8c04300bc629bb7334d523f34e35637886
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-25 11:13:36 -0500

    Updated Render root for React update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
