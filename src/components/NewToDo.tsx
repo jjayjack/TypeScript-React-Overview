16cc9593a284b9f03b52af15190566fc5a4189ff
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-25 15:18:28 -0500

    Adding Cascading Style Sheets to app

--- src/components/NewToDo.tsx
+++ src/components/NewToDo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
   onAddTodo: (todoText: string) => void;
@@ -13,7 +14,7 @@ const NewToDo: React.FC<NewTodoProps> = (props) => {
   };
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">ToDo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
