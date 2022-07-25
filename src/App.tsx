3032a4e89429fc11975f8aa7bab57d87c91adbbe
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-25 11:34:57 -0500

    Rendering list to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ToDoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
 };
 
 export default App;
